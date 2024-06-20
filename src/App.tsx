1a58ea3c4cb38f8bca780bc7cd3cb248f74e7890
JavascriptAlways <maheshjoshi90@gmail.com>
2024-06-20 20:42:13 +0530

    portfolio end code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Header from './components/Header/Header';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
+import Skills from './components/Skills/Skills';
+import Education from './components/Education/Education';
+import Experience from './components/Experience/Experience';
+import Projects from './components/Projectjs/Projects';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      mahesh
-    </>
-  )
+    <div className="App">
+      <Header />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Education /> 
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
